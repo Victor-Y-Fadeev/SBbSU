72f7ec7356890e03052d4164e032dfd347c72a5f
Victor Y. Fadeev <victor.y.fadeev@gmail.com>
2016-12-01 17:39:01 +0300

    Added user interface

--- course1/semester1/hw9/task2/main.cpp
+++ course1/semester1/hw9/task2/main.cpp
@@ -3,12 +3,28 @@
 
 using namespace std;
 
+void aStar(Map *map)
+{
+	int startX = 0;
+	int startY = 0;
+	int finishX = 0;
+	int finishY = 0;
+	
+	cout << "Enter start coordinates (x, y): ";
+	cin >> startX >> startY;
+	cout << "Enter finish coordinates (x, y): ";
+	cin >> finishX >> finishY;
+	cout << endl;
+
+	aStar(map, startX - 1, startY - 1, finishX - 1, finishY - 1);
+}
+
 int main()
 {
 	Map *map = loadMap("map.txt");
-		
-	aStar(map, 0, 0, 5, 5);
-
+	
+	aStar(map);
+	
 	char *temp = output(map);
 	cout << temp << endl;
 	delete[] temp;
