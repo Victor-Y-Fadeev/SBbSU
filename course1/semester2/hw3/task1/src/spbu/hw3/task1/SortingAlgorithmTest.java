b137638cd0f85fe57c43f42ee41242276589fe99
Victor Y. Fadeev <victor.y.fadeev@gmail.com>
2017-03-14 02:09:35 +0300

    Added "QuickSort.java" & upgrade tests

--- course1/semester2/hw3/task1/src/spbu/hw3/task1/SortingAlgorithmTest.java
+++ course1/semester2/hw3/task1/src/spbu/hw3/task1/SortingAlgorithmTest.java
@@ -4,30 +4,49 @@ import org.junit.Before;
 import org.junit.After;
 import org.junit.Test;
 import org.junit.Assert;
+import java.util.Random;
 
 
 public class SortingAlgorithmTest {
-    private Integer[] expected;
-    private Integer[] actual;
+    private final int ARRAY_SIZE = 32767;
+    private Integer[] testingArray;
 
     @Before
     public void setUpSortingAlgorithm() {
-        expected = new Integer[] { 1, 2, 3, 4, 5 };
-        actual = new Integer[] { 5, 1, 4, 2, 3 };
+        Random randomizer = new Random();
+        testingArray = new Integer[ARRAY_SIZE];
+
+        for (int i = 0; i < ARRAY_SIZE; i++) {
+            testingArray[i] = randomizer.nextInt();
+        }
     }
 
     @After
     public void tearDownSortingAlgorithm() {
-        expected = null;
-        actual = null;
+        testingArray = null;
     }
 
     @Test
     public void testInsertionSort() {
         SortingAlgorithm<Integer> testing = new InsertionSort<>();
 
-        testing.sort(actual);
+        testing.sort(testingArray);
 
-        Assert.assertEquals("This is the end, Insertion Sort doesn't work!", expected, actual);
+        checkArray("This is the end, Insertion Sort doesn't work!", testingArray);
+    }
+
+    @Test
+    public void testQuickSort() {
+        SortingAlgorithm<Integer> testing = new QuickSort<>();
+
+        testing.sort(testingArray);
+
+        checkArray("We have some problems with Quick Sort", testingArray);
+    }
+
+    private void checkArray(String message, Integer[] array) {
+        for (int i = 0; i < array.length - 1; i++){
+            Assert.assertTrue(message, array[i].compareTo(array[i + 1]) <= 0);
+        }
     }
 }
