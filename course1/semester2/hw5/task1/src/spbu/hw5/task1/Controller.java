f077a375ac5f6d39480a4091842fcf0a96c1f2d0
Victor Y. Fadeev <victor.y.fadeev@gmail.com>
2017-03-29 01:13:59 +0300

    Separated program logic, added JavaDoc comment & created tests

--- course1/semester2/hw5/task1/src/spbu/hw5/task1/Controller.java
+++ course1/semester2/hw5/task1/src/spbu/hw5/task1/Controller.java
@@ -2,12 +2,16 @@ package spbu.hw5.task1;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.*;
-import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 
-public class Controller {
+/** Controller class for JavaFX. */
+public class Controller implements Initializable {
+    private Calculator calculator;
     @FXML
     private MenuButton menu;
     @FXML
@@ -17,48 +21,42 @@ public class Controller {
     @FXML
     private TextField result;
 
-    public void changeValue(KeyEvent keyEvent) {
-        count();
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        calculator = new Calculator();
+        int minValue = calculator.getMinValue();
+        int maxValue = calculator.getMaxValue();
+
+        firstValue.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(minValue, maxValue));
+        secondValue.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(minValue, maxValue));
     }
 
+    /** Recount result value after menu changes. */
     public void changeOperation(ActionEvent actionEvent) {
         MenuItem temp = (MenuItem) actionEvent.getSource();
         menu.setText(temp.getText());
-        count();
+        setResult();
     }
 
+    /** Recount result value after Spinner changes. */
     public void clickSpinner(MouseEvent mouseEvent) {
-        Spinner spinner = (Spinner) mouseEvent.getSource();
-
-        if (spinner.getValueFactory() == null) {
-            spinner.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 9));
-        }
-
-        count();
+        setResult();
     }
 
-    private void count() {
-        if ((menu.getText().isEmpty()) || (firstValue.getValueFactory() == null) || (secondValue.getValueFactory() == null)) {
-            result.setText("");
+    private void setResult() {
+        if (menu.getText() == "") {
             return;
         }
 
         int firstNumber = (Integer) firstValue.getValue();
         int secondNumber = (Integer) secondValue.getValue();
 
-        switch (menu.getText()) {
-            case "+":
-                result.setText(Integer.toString(firstNumber + secondNumber));
-                break;
-            case "-":
-                result.setText(Integer.toString(firstNumber - secondNumber));
-                break;
-            case "*":
-                result.setText(Integer.toString(firstNumber * secondNumber));
-                break;
-            case "/":
-                result.setText(Integer.toString(firstNumber / secondNumber));
-                break;
+        if ((menu.getText().compareTo("/") == 0) && (secondNumber == 0)) {
+            result.setText("");
+            return;
         }
+
+        int answer = calculator.count(menu.getText(), firstNumber, secondNumber);
+        result.setText(Integer.toString(answer));
     }
 }
