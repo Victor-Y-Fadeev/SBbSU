2268942e7692b0344a5fb161bdd4d04c51f75041
Victor Y. Fadeev <victor.y.fadeev@gmail.com>
2017-03-29 05:09:26 +0300

    Complete "StackCalculator.java"

--- course1/semester2/hw5/task2/src/spbu/hw5/task2/Controller.java
+++ course1/semester2/hw5/task2/src/spbu/hw5/task2/Controller.java
@@ -1,6 +1,25 @@
 package spbu.hw5.task2;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
+
+/** Controller class for JavaFX. */
 public class Controller {
+    private StackCalculator calculator;
+    @FXML
+    private TextField display;
 
+    public Controller() {
+        calculator = new StackCalculator();
+    }
+
+    public void clickButton(ActionEvent actionEvent) {
+        Button button = (Button) actionEvent.getSource();
+
+        calculator.computing(button.getText());
+        display.setText(calculator.getOutput());
+    }
 }
