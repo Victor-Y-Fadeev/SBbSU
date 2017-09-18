a9c47f042bf2f6d86b9180ee0837024b7f124001
Victor Y. Fadeev <victor.y.fadeev@gmail.com>
2017-09-19 01:44:43 +0300

    Start work with Computer class

--- course2/sem3/hw2/task1/src/main/java/sem3/hw2/task1/Computer.java
+++ course2/sem3/hw2/task1/src/main/java/sem3/hw2/task1/Computer.java
@@ -1,5 +1,30 @@
 package sem3.hw2.task1;
 
+import java.util.Random;
+
 /** Computer class. */
 public class Computer {
+    public final String OS;
+    public boolean isVirus;
+    private static final double WINDOWS = 0.7;
+    private static final double LINUX = 0.1;
+    private static final double MAC = 0.3;
+    private final double DEFECT;
+
+    public Computer(String System) {
+        isVirus = false;
+        OS = System;
+
+        if (OS.equals("Windows")){
+            DEFECT = WINDOWS;
+        } else if (OS.equals("Linux")) {
+            DEFECT = LINUX;
+        } else {
+            DEFECT = MAC;
+        }
+    }
+
+    public void tryInfect() {
+        //Rundom rundomizer = new Rundom();
+    }
 }
