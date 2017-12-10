6df9f157fee295ab7b0832625dc38143b20d44ba
Victor Y. Fadeev <victor.y.fadeev@gmail.com>
2017-12-11 02:07:03 +0300

    Homework 3 (task 1)

---
+++ course2/sem3/hw3/task1/src/main/java/sem3/hw3/task1/Controller.java
@@ -0,0 +1,32 @@
+package sem3.hw3.task1;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+
+/** Controller class for JavaFX. */
+public class Controller {
+    private TicTacToe game;
+
+    /** Create controller. */
+    public Controller() {
+        game = new TicTacToe();
+    }
+
+    /** Make a game move. */
+    public void clickButton(ActionEvent actionEvent) {
+        Button button = (Button) actionEvent.getSource();
+
+        Integer rowIndex = GridPane.getRowIndex(button);
+        Integer columnIndex = GridPane.getColumnIndex(button);
+
+        if (rowIndex == null) {
+            rowIndex = 0;
+        }
+        if (columnIndex == null) {
+            columnIndex = 0;
+        }
+
+        button.setText(game.toMove(rowIndex, columnIndex));
+    }
+}
---
+++ course2/sem3/hw3/task1/src/main/java/sem3/hw3/task1/TicTacToe.java
@@ -0,0 +1,79 @@
+package sem3.hw3.task1;
+
+/** Tic-tac-toe logic. */
+public class TicTacToe {
+    private boolean isMoveX;
+    private boolean isWin;
+    private int[][] matrix;
+
+    /** Create game. */
+    public TicTacToe() {
+        isMoveX = true;
+        isWin = false;
+
+        matrix = new int[3][3];
+        for(int i = 0; i < 3; i++) {
+            for(int j = 0; j < 3; j++) {
+                matrix[i][j] = -1;
+            }
+        }
+    }
+
+    /** Make a game move. */
+    public String toMove(int row, int column) {
+        if (isWin || (matrix[row][column] != -1)) {
+            return getText(matrix[row][column]);
+        }
+
+        matrix[row][column] = isMoveX ? 1 : 0;
+        isMoveX = !isMoveX;
+
+        checkWin();
+        return getText(isMoveX ? 0 : 1);
+    }
+
+    private String getText(int position) {
+        switch (position){
+            case 0:
+                return "O";
+            case 1:
+                return "X";
+        }
+
+        return "";
+    }
+
+    private void checkWin() {
+        if (matrix[1][1] != -1) {
+            boolean firstDiagonal = true;
+            boolean secondDiagonal = true;
+
+            for (int i = 0; i < 2; i++) {
+                if (matrix[i][i] != matrix[i + 1][i + 1]) {
+                    firstDiagonal = false;
+                }
+
+                if (matrix[2 - i][i] != matrix[1 - i][i + 1]) {
+                    secondDiagonal = false;
+                }
+            }
+
+            if (firstDiagonal || secondDiagonal) {
+                isWin = true;
+                return;
+            }
+        }
+
+        for (int i = 0; i < 3; i++){
+            if ((matrix[0][i] == matrix[1][i]) && (matrix[1][i] == matrix[2][i]) && (matrix[0][i] != -1)) {
+                isWin = true;
+                return;
+            }
+
+            if ((matrix[i][0] == matrix[i][1]) && (matrix[i][1] == matrix[i][2]) && (matrix[i][0] != -1)) {
+                isWin = true;
+                return;
+            }
+        }
+    }
+}
