86a32506436068a42827dfe81a8cd02e808e447c
Victor Y. Fadeev <victor.y.fadeev@gmail.com>
2018-05-28 12:47:22 +0300

    Use concatMap

--- course2/sem4/hw3/task2/main.hs
+++ course2/sem4/hw3/task2/main.hs
@@ -1,4 +1,3 @@
-sequence :: [Int]
-sequence = sequence179 sequence0 where
-    sequence179 (x:xs) = (x * 10 + 1) :((x * 10 + 7) : ((x * 10 + 9) : (sequence179 xs)))
-    sequence0 = 0 : sequenceFin
+order :: [Int]
+order = 1 : 7 : 9 : (concatMap order179 order) where
+    order179 x = [x * 10 + 1, x * 10 + 7, x * 10 + 9]
