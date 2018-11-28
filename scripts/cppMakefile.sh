ff709254cc3914a3e9df9f250dc8941369af2978
Victor Y. Fadeev <victor.y.fadeev@gmail.com>
2018-11-28 05:55:31 +0300

    Generate cpp Makefiles

---
+++ scripts/cppMakefile.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+num=$1
+files=`ls -xm *.cpp`
+main=`grep -rl " main(" .`
+main=${main%".cpp"}
+
+cat << EOF > Makefile
+CC=g++
+CFLAGS=-c -Wall
+LDFLAGS=
+SOURCES=${files//","/""}
+OBJECTS=\$(SOURCES:.cpp=.o)
+EXECUTABLE=${main#"./"}.out
+
+all: \$(SOURCES) \$(EXECUTABLE)
+
+\$(EXECUTABLE): \$(OBJECTS) 
+	\$(CC) \$(LDFLAGS) \$(OBJECTS) -o \$@
+
+.cpp.o:
+	\$(CC) \$(CFLAGS) \$< -o \$@
+
+clean:
+	rm -rf *.o \$(EXECUTABLE)
+EOF
+
+if ! make >/dev/null 2>/dev/null ; then
+	make
+	exit 1;
+fi
+
+#make -f Makefile clean
+
+exit 0
